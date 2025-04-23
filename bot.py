eb631c91f4afa8a58d289ea91afd9ebda656ec67
borntotilt <sashahotx@gmail.com>
2025-04-23 21:10:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiogram.types import ParseMode
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
 
-API_TOKEN = 'your_token_here'
+API_TOKEN = '7581874786:AAEHu6aCqlQVfsFBgWp0eX_mvXwSKlw 7581874786'
 
 # Настроим логирование
 logging.basicConfig(level=logging.INFO)
